90b2264fdc6ac790ada5986f708e158d2f8c571c
Leo Giesen <contact@leogiesen.de>
2023-06-28 20:47:40 +0200

    minor changes to generate data

--- src/generate_data.py
+++ src/generate_data.py
@@ -9,6 +9,17 @@ path_images = f'{root_path}images/'
 path_masks = f'{root_path}masks/'
 
 def get_files(dir):
+    """
+    Get all files from a directory
+
+    Parameters
+    ----------
+    dir: Array 
+
+    Returns
+    -------
+    Array of strings
+    """
     return [f for f in listdir(dir) if isfile(join(dir, f))]
 
 def extract_data(data_filenames):
@@ -17,7 +28,7 @@ def extract_data(data_filenames):
 
     Parameters
     ----------
-    data_filenames: Array 
+    data_filenames: Array of strings
     Path to the train data (default: None)
     root_path + filename = complete filepath 
 
@@ -26,26 +37,31 @@ def extract_data(data_filenames):
     dataset: Tuple of np.ndarray
     """
     # unzip data
-    for filename in data_filenames:
+    for idx, filename in enumerate(data_filenames):
+        if filename[0][-4:] != '.zip':
+            print(f'{filename} cannot be extracted because it is not a zip file.')
+            continue;
+        data_filenames[idx] = filename.replace('.zip','')
+        mask_or_img = 'masks' if 'mask' in filename else 'images'
         with ZipFile(filename, 'r') as zObject:
-            zObject.extractall(path=f"{root_path}{filename.replace('.zip','')}")
-
-    # load satellite images
-    X = np.load(f'/content/images/{get_files(path_images)[0]}')
+            zObject.extractall(path=f"{root_path}{mask_or_img}/{filename}")
+    
+    # load satellite images by loading the first one and then concatenating the rest
+    X = np.load(f'{path_images}{get_files(path_images)[0]}')
     for filename in get_files(path_images)[1:]:
-        temp = np.load(f'/content/images/{filename}', allow_pickle=True)
+        temp = np.load(f'{path_images}{filename}', allow_pickle=True)
         X = np.concatenate((X, temp))
-    # load labels
-    y = np.load(f'/content/masks/{get_files(path_masks)[0]}')
+    # load labels by loading the first one and then concatenating the rest
+    y = np.load(f'{path_masks}{get_files(path_masks)[0]}')
     for filename in get_files(path_masks)[1:]:
-        temp = np.load(f'/content/masks/{filename}', allow_pickle=True)
+        temp = np.load(f'{path_masks}{filename}', allow_pickle=True)
         y = np.concatenate((y, temp))
-    del temp
 
     # ceil the values at 2000 because clouds have a different reflection value
     ceiling = 2000
     X[X > ceiling] = ceiling
     #scale values between 0 and 1
     X = X / ceiling
+    del mask_or_img, temp, ceiling
     
     return (X, y)
