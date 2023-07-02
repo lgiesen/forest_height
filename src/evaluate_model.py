887f1e29dad2d31b0974fc61c3fb9366755ab20d
Leo Giesen <contact@leogiesen.de>
2023-07-02 13:53:45 +0200

    :bug: fix rmse calculation

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -6,7 +6,7 @@ from sklearn.metrics import (mean_absolute_error,
 def evaluate_model(y_test, y_pred):
     mae = mean_absolute_error(y_test, y_pred)
     mse = mean_squared_error(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred) ** (1/2)
+    rmse = mae ** (1/2)
     mape = mean_absolute_percentage_error(y_test, y_pred)
     print(f'MAE: {mae}; MSE: {mse}; RMSE: {rmse}; MAPE: {mape}')
     return (mae, mse, rmse, mape)
