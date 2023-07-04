9592f4a1ce2112720e95c344abf4adf0f67658ab
Leo Giesen <contact@leogiesen.de>
2023-07-04 21:03:15 +0200

    🚧 test out imports

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,6 +1,5 @@
 import joblib
 import matplotlib.pyplot as plt
-from forest_height.src.load_data import load_data
 from matplotlib.ticker import StrMethodFormatter
 from sklearn.metrics import (mean_absolute_error,
                              mean_absolute_percentage_error,
