21568bf66adb4c976caa2df8d46273d5793abb6d
Leo Giesen <contact@leogiesen.de>
2023-07-04 21:05:16 +0200

    :fire: rm duplicate function file

--- src/current_variables.py
+++
@@ -1,8 +0,0 @@
-# print how much space each variable requires
-from __future__ import print_function  # for Python2
-
-import sys
-
-local_vars = list(locals().items())
-for var, obj in local_vars:
-    print(var, sys.getsizeof(obj))
