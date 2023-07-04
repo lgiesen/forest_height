b301a6e0b0096f5b6d3fa81b107670334cc63f78
Leo Giesen <contact@leogiesen.de>
2023-07-04 19:46:01 +0200

    :sparkles: feature importance + pred vs true function

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
 from sklearn.metrics import (mean_absolute_error,
                              mean_absolute_percentage_error,
                              mean_squared_error)
 
+color = "#01748F"
 
 def evaluate_model(y_test, y_pred):
     mae = mean_absolute_error(y_test, y_pred)
@@ -10,3 +13,48 @@ def evaluate_model(y_test, y_pred):
     mape = mean_absolute_percentage_error(y_test, y_pred)
     print(f'MAE: {mae}; MSE: {mse}; RMSE: {rmse}; MAPE: {mape}')
     return (mae, mse, rmse, mape)
+
+
+
+def feature_importance(model, model_name, cols=['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12', 'NDVI', 'EVI', 'SAVI', 'IRECI', 's2rep']):
+    importance = model.feature_importances_
+    # summarize feature importance
+    for i,v in enumerate(importance):
+        print('Feature: %0d, Score: %.5f' % (i,v))
+    # plot feature importance
+    plt.bar(cols, importance, color=color)
+    plt.xlabel("Features")
+    plt.ylabel("Feature Importance")
+    plt.title(f"Feature Importance of {model_name} Regression")
+    plt.show()
+
+def pred_vs_true(model_name, ds="all"):
+    # get necessary data
+    X_train, y_train, X_test, y_test = load_data(ds)
+    y_pred = gbr.predict(X_test)
+    
+    # visualize predictions vs. true labels
+    fig = plt.figure(figsize=(6,6))
+    plt.scatter(y_pred, y_test, color=color, alpha=0.5)
+    plt.xticks(rotation=45)
+    plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}'))
+    plt.plot([-1,75], [-1, 75], 'k--')
+    plt.xlabel("Predictions")
+    plt.ylabel("True Labels")
+    plt.xlim([-1,75])
+    plt.ylim([-1,75])
+    plt.title(f"{model_name} Regression: Prediction vs. Labels")
+    plt.show()
+
+    # only the NDVI channel is plotted on the x-axis
+    # because 11-dimensional data cannot be visualized for humans
+    channel = 10 if X_test.shape[1] > 9 else 0
+    fig, ax = plt.subplots()
+    plt.scatter(X_test[:,10], y_test, 10, color='black')
+    plt.scatter(X_test[:,10], y_pred, 10, color=color)
+    plt.title(f'{model_name} Regression: NDVI and Forest Height')
+    if channel == 10:
+        plt.xlabel('NDVI Value')
+    plt.ylabel('Forest Height')
+    ax.legend(("True Value", "Prediction"), loc='upper left')
+    plt.show()
--- src/predict_data.py
+++ src/predict_data.py
@@ -1,33 +1,41 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 def load_img(filepath, features):
     X = np.load(filepath)
     color_channels, width, height = X.shape
-    Xr = X.reshape(10,-1).transpose()
-    #X_flat = np.reshape(X, (width * height, color_channels))
-    if False:
-        if "color_channels" in features:
-            ceiling = 2000
-            X[X > ceiling] = ceiling
-            #scale values between 0 and 1
-            X = X / ceiling
-            color_channels, width, height = X.shape
-            X = np.reshape(X, (width*height, color_channels))
-        if "NDVI" in features:
-            # define columns to prepare NDVI calculation
-            X_flat = pd.DataFrame(X_flat, columns = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12'])
-            # get NDVI value
-            X_NDVI = calculate_ndvi(X_flat)["NDVI"]
-            if "color_channels" in features:
-                return np.concatenate((X, X_NDVI))
-            # reshape data and format it for model predictions
-            return np.array(np.reshape(pd.DataFrame(X_NDVI), (-1,1)))
+    Xr = X.reshape(color_channels,-1).transpose()
+    #if "color_channels" in features:
+    ceiling = 2000
+    # counteract high reflection values of clouds
+    Xr[Xr > ceiling] = ceiling
+    X[X > ceiling] = ceiling
+    # counteract significant negative vegetation indices
+    Xr[Xr < -1*ceiling] = -1*ceiling
+    X[X < -1*ceiling] = -1*ceiling
+    assert len(X.shape) == 3
+    color_channels, width, height = X.shape
+    Xr = np.reshape(Xr, (width*height, color_channels))
+    Xr = pd.DataFrame(Xr, columns = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12'])
+    if "NDVI" in features:
+        # get NDVI value
+        Xr = calculate_ndvi(Xr)
+        if features == ["NDVI"]:
+            Xr = Xr["NDVI"]
+    if "VI" in features:
+        Xr = calculate_VIs(Xr)
+        if features == ["VI"]:
+            vi_cols = ["EVI", "SAVI", "IRECI", "s2rep"]
+            Xr = X_VI[vi_cols]
+        elif features == ["NDVI", "VI"]:
+            Xr = Xr[["NDVI", "EVI", "SAVI", "IRECI", "s2rep"]]
+    scaler = StandardScaler()
+    X, Xr = scaler.fit_transform(Xr), scaler.fit_transform(X)
     return X, Xr, (color_channels, width, height)
 
-def pred_img(filepath, features, model, plot_img=True):
-    X, Xr, (color_channels, width, height) = load_img(filepath, features)
+def pred_img(filepath, features, model, plot_img=True, scale=True):
     pred = model.predict(Xr)
     pred = pred.transpose().reshape(1, width, height)
     if plot_img:
